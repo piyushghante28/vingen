24ecd93bd77a7ad5465e9c352c27c569910d91b6
piyushghante28 <120301521+piyushghante28@users.noreply.github.com>
2025-02-21 11:43:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ log = st.sidebar.empty()
 log_data = []
 
 # Display buttons in a single row
-grid_columns = st.columns(len(WMI_CODES)* 4)
+grid_columns = st.columns(len(WMI_CODES))
 
 for index, (manufacturer, wmi) in enumerate(WMI_CODES.items()):
     with grid_columns[index]:
