9515543371a2cff1d4a2dbb17da1fc5c3a1feaca
piyushghante28 <120301521+piyushghante28@users.noreply.github.com>
2025-02-21 14:15:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import aiohttp
 import asyncio
 
 # Apply Dark Theme with Monospace Font
-st.set_page_config(page_title="VIN Generator", layout="centered",page_icon="https://cdn-icons-png.flaticon.com/512/846/846338.png",font="monospace")
+st.set_page_config(page_title="VIN Generator", layout="centered",page_icon="https://cdn-icons-png.flaticon.com/512/846/846338.png")
 
 
 # Define Manufacturer WMI codes
